17cd87eb6f9e54bb844285d1c26c2ce20400e6c4
artist <artist@studyGit.com>
2016-09-25 13:06:17 +0900

    Initial Commit

---
+++ evvDetectColorAndFontObj/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  evvDetectColorAndFontObj
+//
+//  Created by artist on 9/25/16.
+//  Copyright © 2016 EddieKwon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
